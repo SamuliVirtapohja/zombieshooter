a7d7838ec01997a2c953f088c9fbca53596685c1
L4721 <l4721@student.jamk.fi>
2019-04-23 21:46:22 +0300

    sekaan

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    GameObject player;
+
+    public float speedH = 2f;
+    public float speedV = 2f;
+
+    private float yaw = 0f;
+    private float pitch = 0f;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponent<PlayerControl>().gamePaused)
+        {
+
+        }
+        else
+        {
+            Vector3 campos = new Vector3(player.transform.position.x, player.transform.position.y + 1, player.transform.position.z);
+            transform.position = campos;
+            RotateCamera();
+        }
+    }
+
+    void RotateCamera()// camera rotation for first person
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        Vector3 rotateCamera = new Vector3(pitch, yaw, 0f);
+
+        transform.eulerAngles = rotateCamera;
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public bool gamePaused = false;
+    public float speed = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LockMouse();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameState();
+        Movement();
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        
+        ParticleSystem gunfire = GameObject.FindGameObjectWithTag("GunFire").GetComponent<ParticleSystem>();
+        gunfire.Play();
+
+        GameObject barrelend = GameObject.FindGameObjectWithTag("Weapon");
+        Debug.Log(Input.mousePosition);
+    }
+
+    void RotatePlayer() // rotate player according to maincamera
+    {
+        transform.rotation = Camera.main.transform.rotation;
+    }
+
+    void Movement()// player movement
+    {
+        RotatePlayer();
+        float moveHorizontally = Input.GetAxis("Horizontal");
+        float moveVertically = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontally, 0 ,moveVertically);
+        transform.Translate(movement * Time.deltaTime * speed);
+    }
+
+    void GameState()// set gamestate and enable menu
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+            {
+                gamePaused = false;
+                Time.timeScale = 1;
+            }
+            else
+            {
+                gamePaused = true;
+                Time.timeScale = 0;
+            }                
+        }       
+    }
+
+    void LockMouse()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void ReleaseMouse()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+}
