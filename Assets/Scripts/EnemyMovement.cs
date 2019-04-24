0c412180446f3da87adc35f16d34ab7d2d0416a5
Olli <olli.rampanen3@gmail.com>
2019-04-24 14:33:50 +0300

    Enemy movement

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,21 +6,24 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     Transform player;
-    PlayerHealth playerHealth;
-    EnemyHealth enemyHealth;
+    //PlayerHealth playerHealth;
+    //EnemyHealth enemyHealth;
     UnityEngine.AI.NavMeshAgent nav;
 
     void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        playerHealth = player.GetComponent<PlayerHealth>();
-        enemyHealth = GetComponent<EnemyHealth>();
+        //playerHealth = player.GetComponent<PlayerHealth>();
+        //enemyHealth = GetComponent<EnemyHealth>();
         nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
-
-
     void Update()
+    {
+        nav.SetDestination(player.position);
+    }
+
+    /*void Update()
     {
         if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
         {
@@ -30,8 +33,8 @@ public class EnemyMovement : MonoBehaviour
         {
             nav.enabled = false;
         }
-    }
-    
+    }*/
+
 }
 
 
