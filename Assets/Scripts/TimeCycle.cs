10ff38b33d730fe8ba0081b70c4ffede052847a2
Olli <olli.rampanen3@gmail.com>
2019-04-24 16:58:29 +0300

    Timecycle and crosshair

---
+++ Assets/Scripts/TimeCycle.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCycle : MonoBehaviour
+{
+    public static int min;
+    public static float sec;
+    public static int cycle;
+
+    Text text;
+
+    void Start()
+    {
+        InvokeRepeating("Time", 1f, 1f);
+    }
+
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        min = 0;
+        sec = 10;
+        cycle = 1;
+    }
+
+    void Time()
+    {
+        
+        sec= sec - 1;
+
+        if (sec < 0 && min > 0)
+        {
+
+            min--;
+            sec = 60;
+        }
+
+
+        else if (min == 0 && sec <= 0)
+        {
+
+            min = 4;
+            sec = 60;
+            if (cycle == 1)
+                cycle = 2;
+            else if (cycle == 2)
+                cycle = 1;
+        }
+
+        if (cycle == 1)
+        {
+            text.text = "Day remaining: " + min + ":" + sec;
+        }
+        else
+        {
+            text.text = "Night remaining: " + min + ":" + sec;
+        }
+
+    }
+   
+
+
+}
