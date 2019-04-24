b252172d995911669b9f87642bf0429471745070
Samuli Virtapohja <samuli.virtapohja@gmail.com>
2019-04-24 04:31:19 +0300

    CameraControl
    Add camera rotation restrictions on pitch

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -32,13 +32,7 @@ public class CameraControl : MonoBehaviour
         {
             RotateCamera();
             Vector3 campos = new Vector3(player.transform.position.x, player.transform.position.y + 1, player.transform.position.z);
-            transform.position = campos;
-            xrot = player.transform.rotation.x;
-            if (xrot < -45)
-            {
-                xrot = -45;
-            }
-            
+            transform.position = campos;            
         }
     }
 
@@ -47,6 +41,15 @@ public class CameraControl : MonoBehaviour
         yaw += speedH * Input.GetAxis("Mouse X");
         pitch -= speedV * Input.GetAxis("Mouse Y");
 
+        if (pitch < -10)
+        {
+            pitch = -10;
+        }
+
+        if (pitch > 10)
+        {
+            pitch = 10;
+        }
         Vector3 rotateCamera = new Vector3(pitch, yaw, 0f);
 
         transform.eulerAngles = rotateCamera;
